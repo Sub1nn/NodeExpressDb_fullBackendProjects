df827506a39fd12ca35ceaccd614aa421110b393
Subin <sub_1n@yahoo.com>
2023-12-04 22:59:53 +0200

    adjustments made

--- nodeWithDb_Project1/index.js
+++ nodeWithDb_Project1/index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { dbConnect } from "./db.connect.js";
-
+import teacherRoutes from "./teacher.routes.js";
 const app = express();
 
 app.use(express.json());
+app.use(teacherRoutes);
 dbConnect();
 
 const port = 8001;
