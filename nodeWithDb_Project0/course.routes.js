e79ea6e0ebf4044437c7753c20e6549cbcde6360
Subin <sub_1n@yahoo.com>
2023-12-10 20:27:18 +0200

    get and delete route implemented

--- nodeWithDb_Project0/course.routes.js
+++ nodeWithDb_Project0/course.routes.js
@@ -1,14 +1,25 @@
 import express from "express";
 import { Course } from "./course.model.js";
-const router = express.Router();
 import * as Yup from "yup";
 import { checkMongoIdValidity } from "./utils.js";
+
+const router = express.Router();
+
 // add course
-router.post("/course/add", async () => {
+router.post("/course/add", async (req, res) => {
+  const newCourse = req.body;
+
+  //validate courseSchema with Yup
   let courseSchema = Yup.object({
-    name: Yup.string().required().trim(),
-    price: Yup.number().required(),
-    duration: Yup.number().required(),
+    name: Yup.string()
+      .required()
+      .trim()
+      .min(1, "Name must be at least 1 character")
+      .max(55, "Name must not be more than 55 chars long"),
+    price: Yup.number().required().min(1, "Price must be at least 1 euros"),
+    duration: Yup.number()
+      .required()
+      .min(1, "Duration must be  at least 1 day"),
   });
 
   try {
@@ -17,15 +28,14 @@ router.post("/course/add", async () => {
     return res.status(400).send({ message: error.message });
   }
 
-  const newCourse = req.body;
   await Course.create(newCourse);
   res.status(200).send({ message: "New Course Created" });
 });
 
-// get course details
+// get a single course details
 router.get("/course/details/:id", async (req, res) => {
   const courseId = req.params.id;
-
+  // validate for mongo id
   const isValid = checkMongoIdValidity(courseId);
   // if not valid mongo id
   if (!isValid) {
@@ -38,7 +48,43 @@ router.get("/course/details/:id", async (req, res) => {
     return res.status(404).send({ message: "Course not found" });
   }
   // send appropriate response
-  return res.status(200).send("Details");
+  return res.status(200).send(course);
+});
+
+// get all course details
+router.get("/course", async (req, res) => {
+  try {
+    const courses = await Course.find();
+    res.status(200).send(courses);
+  } catch (error) {
+    res.status(400).send({ message: "Error finding courses" });
+  }
+});
+
+// delete a course
+
+router.delete("course/delete/:id", async (req, res) => {
+  const courseId = req.params.id;
+
+  //   validate for mongo id
+  const isValid = checkMongoIdValidity(courseId);
+
+  //   if not valid mongo id
+  if (!isValid) {
+    return res.status(400).send({ message: "Invalid course id." });
+  }
+
+  //   find course
+  const course = await Course.findOne({ _id: courseId });
+
+  //   if not course
+  if (!course) {
+    return res.status(404).send({ message: "Course does not exist." });
+  }
+
+  await Course.deleteOne({ _id: courseId });
+
+  return res.status(200).send({ message: "Course is removed successfully." });
 });
 
 export default router;
