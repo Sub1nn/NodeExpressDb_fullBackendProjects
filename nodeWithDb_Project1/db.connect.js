dc19a0e7e7caa67321aa2c2c6b51bda8efe212b2
Subin <sub_1n@yahoo.com>
2023-12-12 14:11:47 +0200

    mongo connection string added as MONGO_URL

--- nodeWithDb_Project1/db.connect.js
+++ nodeWithDb_Project1/db.connect.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnect = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://sub_1n:maya2046Lai@subin01.bbc2g1d.mongodb.net/narrowway?retryWrites=true&w=majority"
-    );
+    await mongoose.connect(MONGO_URL);
     console.log("Database connection established");
   } catch (error) {
     console.log(error.message);
--- nodeWithDb_Project2/dbConnect.js
+++ nodeWithDb_Project2/dbConnect.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnect = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://sub_1n:maya2046Lai@subin01.bbc2g1d.mongodb.net/narrowway?retryWrites=true&w=majority"
-    );
+    await mongoose.connect(MONGO_URL);
     console.log("Connected to the mongoose DB");
   } catch (error) {
     console.log(error.message);
