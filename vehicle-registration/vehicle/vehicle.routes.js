48bb2f3f83d002e9c2e68315fd44d86279139ad3
Subin <sub_1n@yahoo.com>
2023-12-13 22:45:12 +0200

    middlewares implemented in the routes

--- vehicle-registration/vehicle/vehicle.routes.js
+++ vehicle-registration/vehicle/vehicle.routes.js
@@ -1,26 +1,40 @@
 import express from "express";
 import {
   addVehicle,
+  checkIfVehicleExists,
   deleteVehicle,
   getSingleVehicleDetail,
   getVehicleDetails,
   updateVehicle,
+  validateVehicleData,
+  validateVehicleId,
 } from "./vehicle.service.js";
 
 const router = express.Router();
 export default router;
 
-// create(add/register) a vehicle
-router.post("/add", addVehicle);
+// ? create(add/register) a vehicle
+router.post("/add", validateVehicleData, addVehicle);
 
-// get the list of all vehicles
+// ? get the list of all vehicles
 router.get("/details", getVehicleDetails);
 
-// find a single vehicle
-router.get("/details/:id", getSingleVehicleDetail);
+// ? find a single vehicle
+router.get("/details/:id", validateVehicleId, getSingleVehicleDetail);
 
-// update a vehicle
-router.put("/edit/:id", updateVehicle);
+// ? update a vehicle
+router.put(
+  "/edit/:id",
+  validateVehicleData,
+  validateVehicleId,
+  checkIfVehicleExists,
+  updateVehicle
+);
 
-// delete a vehicle
-router.delete("/delete/:id", deleteVehicle);
+// ? delete a vehicle
+router.delete(
+  "/delete/:id",
+  validateVehicleId,
+  checkIfVehicleExists,
+  deleteVehicle
+);
