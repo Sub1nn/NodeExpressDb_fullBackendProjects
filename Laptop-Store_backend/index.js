4b54b19daf61ee3c16dd2c8c6ed26a3a0601a2f0
Subin <sub_1n@yahoo.com>
2023-12-17 22:02:20 +0200

    small changes

--- Laptop-Store_backend/index.js
+++ Laptop-Store_backend/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import laptopRoutes from "./routes/laptopRoute.js";
 import { dbConnect } from "./config/dbConnection.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
+
 const app = express();
 
 app.use(express.json());
 app.use("/laptop", laptopRoutes);
 dbConnect();
+app.use(errorHandler);
 
 const PORT = 8000;
 
