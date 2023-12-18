c687cb77c3cebf4ec847d36742cfba45a62d2a8b
Subin <sub_1n@yahoo.com>
2023-12-18 21:33:02 +0200

    database connection established

---
+++ todo-app/db.connect.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+const MONGO_URL = process.env.MONGO_URL;
+
+export const connectDb = async () => {
+  try {
+    await mongoose.connect(MONGO_URL);
+    console.log("Data connection successful");
+  } catch (error) {
+    console.log("DB connection failed");
+    console.log(error.message);
+  }
+};
