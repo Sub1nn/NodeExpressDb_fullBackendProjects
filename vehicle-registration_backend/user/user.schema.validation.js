7fabb38cc74dd26b650ec23a1edc814274624289
Subin <sub_1n@yahoo.com>
2023-12-20 21:55:04 +0200

    Yup validation implemented for user schema

---
+++ vehicle-registration_backend/user/user.schema.validation.js
@@ -0,0 +1,28 @@
+import * as Yup from "yup";
+
+export const userDataSchemaValidation = Yup.object({
+  firstName: Yup.string()
+    .required()
+    .trim()
+    .max(55, "name must not exceed 55 characters"),
+  lastName: Yup.string()
+    .required()
+    .trim()
+    .max(65, "name must not exceed 55 characters"),
+  email: Yup.string()
+    .trim()
+    .required()
+    .lowercase()
+    .max(70, "email must be at most 70 characters"),
+  password: Yup.string()
+    .required()
+    .min(4, "password must contain at least 4 characters")
+    .max(25, "password must not exceed 25 characters"),
+  gender: Yup.string().trim().oneOf(["male", "female", "preferNotToSay"]),
+});
+
+export const userEmailSchemaValidation = Yup.string()
+  .email("must be a valid email address")
+  .trim()
+  .required()
+  .lowercase();
