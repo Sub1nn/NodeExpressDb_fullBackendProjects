86fdc21cf2e788734828a22b653630087974ab6a
Subin <sub_1n@yahoo.com>
2023-12-22 22:48:17 +0200

    skip-limit data validation with yup file added

--- todo-app/todo/todo.validation.js
+++ todo-app/todo/todo.validation.js
@@ -7,3 +7,16 @@ export const todoValidationSchema = Yup.object({
   description: Yup.string().required().trim().max(55),
   date: Yup.date().min(currentDate).required(),
 });
+
+// skip-limit data validation
+
+export const getTodoListValidationSchema = Yup.object({
+  page: Yup.number("Page must be a number")
+    .min(1, "Page must be at least 1")
+    .integer("Page must be an integer")
+    .default(1),
+  limit: Yup.number()
+    .min(1, "Limit must be at least 1")
+    .integer("Limit must be an integer")
+    .default(1),
+});
