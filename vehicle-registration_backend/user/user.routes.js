58514f58edb044cda9a499973b8b573957c682c3
Subin Khatiwada <Subinkhatiwada@gmail.com>
2023-12-27 21:22:48 +0200

    updated some file imports

--- vehicle-registration_backend/user/user.routes.js
+++ vehicle-registration_backend/user/user.routes.js
@@ -1,18 +1,26 @@
 import { Router } from "express";
 
+import { validateReqBody } from "../middlewares/validation.middleware.js";
 import {
-  loginInUser,
-  registerUser,
-  validateEmail,
-  validateNewUser,
-} from "./user.service.js";
+  userDataSchemaValidation,
+  // userEmailSchemaValidation,
+} from "./user.schema.validation.js";
+import { loginInUser, registerUser } from "./user.service.js";
 
 const router = Router();
 
 // ? register new user
-router.post("/user/register", validateNewUser, registerUser);
+router.post(
+  "/user/register",
+  validateReqBody(userDataSchemaValidation),
+  registerUser
+);
 
 // ? login user
-router.post("/user/login", validateEmail, loginInUser);
+router.post(
+  "/user/login",
+  // validateReqBody(userEmailSchemaValidation),
+  loginInUser
+);
 
 export default router;
