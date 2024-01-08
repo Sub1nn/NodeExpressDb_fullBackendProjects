f36ef3a3f9a884fe23527a0bea63514b1f1d0fa8
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-01-08 21:55:46 +0200

    implemented cors

--- todo-app/index.js
+++ todo-app/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import { connectDb } from "./db.connect.js";
 import todoRoutes from "./todo/todo.routes.js";
 import userRoutes from "./user/user.routes.js";
+import cors from "cors";
 
 const app = express();
 
 // make app understand json
 app.use(express.json());
+app.use(cors());
 
 // connect db
 connectDb();
