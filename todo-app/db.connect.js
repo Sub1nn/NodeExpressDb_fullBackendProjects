b2a3f4d4091c60dd22f1864c75e6a40bf152af33
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-01-08 21:55:23 +0200

    updated the connection url

--- todo-app/db.connect.js
+++ todo-app/db.connect.js
@@ -1,12 +1,18 @@
 import mongoose from "mongoose";
-const MONGO_URL = process.env.MONGO_URL;
+
+const dbName = process.env.DB_NAME;
+const dbPass = encodeURIComponent(process.env.DB_PASSWORD);
+const dbUserName = process.env.DB_USERNAME;
 
 export const connectDb = async () => {
   try {
-    await mongoose.connect(MONGO_URL);
-    console.log("Data connection successful");
+    await mongoose.connect(
+      `mongodb+srv://${dbUserName}:${dbPass}@subin01.bbc2g1d.mongodb.net/${dbName}?retryWrites=true&w=majority`
+    );
+
+    console.log("DB connection established...");
   } catch (error) {
-    console.log("DB connection failed");
+    console.log("DB connection failed...");
     console.log(error.message);
   }
 };
